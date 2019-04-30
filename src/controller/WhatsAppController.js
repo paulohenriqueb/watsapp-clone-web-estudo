8c9696f422a3f3391aa33cc5efbea5c047802696
Paulo henrique <paulohenriqueb.silva@gmail.com>
2019-04-30 17:41:20 -0300

    adicionado a funcao elementsPrototype porém não finalizada

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,11 +1,29 @@
 class WhatsAppController{
 	constructor(){
+
+		this.elementsPrototype();
 		this.loadElements();
+
 	}
 	loadElements(){
 		this.el = {};
 		document.querySelectorAll('[id]').forEach(element=>{
+
 			this.el[Format.getCamelCase(element.id)] = element;
+
 		})
+	}
+	elementsPrototype(){
+
+		Element.prototype.hide = function(){
+			this.style.display = 'none';
+		}
+		Element.prototype.show = function(){
+			this.style.display = 'block';
+		}
+
+		Element.prototype.toggle = function(){
+			this.style.display = (this.style.display ==='none') ? 'block' : 'none';
+		}
 	}
 }
